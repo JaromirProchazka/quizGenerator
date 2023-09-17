e75263e4d93e1320fce85d73fe4c77c738922dd1
Jaromir Prochazka <jaromir.prochazka03@gmail.com>
2023-09-17 17:11:58 +0200

    Add questions colorings

--- quizGenerator/bin/Debug/.sources/script.js
+++ quizGenerator/bin/Debug/.sources/script.js
@@ -11,16 +11,22 @@ function ShowAnswear(id) {
     n.querySelector(".question_answer").style.display = "block";
   }
 }
+
 function ShowQuestion(id) {
   n = document.querySelector("#" + id);
   if (n.classList.contains("heading_sections")) {
+    n.querySelector(".section_heading").style.color = "tomato";
     n = n.querySelector(".section_heading");
+  } else {
+    n.querySelector(".question_name").style.color = "tomato";
   }
   n.style.display = "block";
 }
+
 function HideQuestion(id) {
   n = document.querySelector("#" + id);
   if (n.classList.contains("heading_sections")) {
+    n.querySelector(".section_heading").style.color = "black";
     n.querySelector(".heading_section_contents").style.display = "none";
     qs = n.querySelectorAll(".question_box");
     for (let i = 0; i < qs.length; i++) {
@@ -30,7 +36,7 @@ function HideQuestion(id) {
     n = n.querySelector(".section_heading");
   } else {
     n.querySelector(".question_answer").style.display = "none";
-    console.log("problem");
+    n.querySelector(".question_name").style.color = "black";
   }
   n.style.display = "none";
 }
