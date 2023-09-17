429a05200517ef7be90ddec7d135225b1bbf003c
Jaromir Prochazka <jaromir.prochazka03@gmail.com>
2023-09-17 16:48:06 +0200

    Fix Headings answers styles

--- quizGenerator/bin/Debug/.sources/script.js
+++ quizGenerator/bin/Debug/.sources/script.js
@@ -1,32 +1,36 @@
 function ShowAnswear(id) {
-    n = document.querySelector("#" + id);
-    if (n.classList.contains("heading_sections")) {
-      for (let el of n.querySelectorAll(".question_box")) {
-        el.style.display = "block";
-        el.querySelector(".question_answer").style.display = "block";
-      }
-    } else {
-      n.querySelector(".question_answer").style.display = "block";
+  n = document.querySelector("#" + id);
+  if (n.classList.contains("heading_sections")) {
+    n.querySelector(".heading_section_contents").style.display = "block";
+    qs = n.querySelectorAll(".question_box");
+    for (let i = 0; i < qs.length; i++) {
+      qs[i].style.display = "block";
+      qs[i].querySelector(".question_answer").style.display = "block";
     }
+  } else {
+    n.querySelector(".question_answer").style.display = "block";
   }
+}
 function ShowQuestion(id) {
-    n = document.querySelector("#" + id);
-    if (n.classList.contains("heading_sections")) {
-        n = n.querySelector(".section_heading");
-    }
-    n.style.display = "block";
+  n = document.querySelector("#" + id);
+  if (n.classList.contains("heading_sections")) {
+    n = n.querySelector(".section_heading");
+  }
+  n.style.display = "block";
 }
 function HideQuestion(id) {
-    n = document.querySelector("#" + id);
-    if (n.classList.contains("heading_sections")) {
-        for (let el of n.querySelectorAll(".question_box")) {
-        el.style.display = "none";
-        el.querySelector(".question_answer").style.display = "none";
-        }
-        n = n.querySelector(".section_heading");
-    } else {
-        n.querySelector(".question_answer").style.display = "none";
-        console.log("problem");
+  n = document.querySelector("#" + id);
+  if (n.classList.contains("heading_sections")) {
+    n.querySelector(".heading_section_contents").style.display = "none";
+    qs = n.querySelectorAll(".question_box");
+    for (let i = 0; i < qs.length; i++) {
+      qs[i].style.display = "none";
+      qs[i].querySelector(".question_answer").style.display = "none";
     }
-    n.style.display = "none";
+    n = n.querySelector(".section_heading");
+  } else {
+    n.querySelector(".question_answer").style.display = "none";
+    console.log("problem");
+  }
+  n.style.display = "none";
 }
